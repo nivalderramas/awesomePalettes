07276336b51ccacd6efa9344bdfbf835de474ee7
Ariel Calzada <ariel.calzada@gmail.com>
2020-08-29 18:12:25 -0500

    Subo cambios

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import clusterColor as cc
 import themeEditor as te
 
-wallpaperPath = ("/home/nico/.config/awesome/wallpapers/galaxy.jpg")
-themePath = ("../themeTest.lua")
-themeVariables = ["theme.bg_normal","theme.bg_focus","theme.bg_urgent","theme.bg_minimize","theme.fg_normal"]
+wallpaperPath = "/home/nico/.config/awesome/wallpapers/galaxy.jpg"
+themePath = "../themeTest.lua"
+themeVariables = [
+    "theme.bg_normal",
+    "theme.bg_focus",
+    "theme.bg_urgent",
+    "theme.bg_minimize",
+    "theme.fg_normal",
+]
 baseColors = len(themeVariables)
-colors = cc.extractColors(wallpaperPath,baseColors,1,False)
-te.editTheme(themePath,colors,themeVariables,wallpaperPath)
+colors = cc.extractColors(wallpaperPath, baseColors, 1, False)
+te.editTheme(themePath, colors, themeVariables, wallpaperPath)
--- themeEditor.py
+++ themeEditor.py
@@ -1,16 +1,18 @@
 import os
-def editTheme(themePath, colors, themeVariables,wallpaperPath):
+
+
+def editTheme(themePath, colors, themeVariables, wallpaperPath):
     themeFile = open(themePath)
     numberColors = len(themeVariables)
-    tmpTheme = open("tmpTheme.lua","w")
+    tmpTheme = open("tmpTheme.lua", "w")
     for line in themeFile:
         lineWritten = False
         line = line.split()
         for i in range(numberColors):
             try:
-                #Search for theme Variables to change, if exists then update it
+                # Search for theme Variables to change, if exists then update it
                 idx = line.index(themeVariables[i])
-                if idx==0:
+                if idx == 0:
                     line[2] = '"' + colors[i] + '"'
                     lineWritten = True
                     tmpTheme.write(" ".join(line))
@@ -18,13 +20,15 @@ def editTheme(themePath, colors, themeVariables,wallpaperPath):
             except ValueError:
                 pass
         if not lineWritten:
-            if len(line)!=0 and line[0] == "theme.wallpaper":
-                line [2] = '"'+wallpaperPath+'"'
+            if len(line) != 0 and line[0] == "theme.wallpaper":
+                line[2] = '"' + wallpaperPath + '"'
             tmpTheme.write(" ".join(line))
             tmpTheme.write("\n")
     tmpTheme.close()
     themeFile.close()
-    os.rename("tmpTheme.lua",themePath)
-#colors=["#000000"]*5
-#themeVariables = ["theme.fg_normal","theme.bg_normal","theme.bg_focus","theme.bg_urgent","theme.bg_minimize"]
-#editTheme("themeExample.lua",colors,themeVariables)
+    os.rename("tmpTheme.lua", themePath)
+
+
+# colors=["#000000"]*5
+# themeVariables = ["theme.fg_normal","theme.bg_normal","theme.bg_focus","theme.bg_urgent","theme.bg_minimize"]
+# editTheme("themeExample.lua",colors,themeVariables)
